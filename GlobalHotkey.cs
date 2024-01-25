477715fa6358f2c2c8df0511bd4618cae91ed5aa
Finlay Christ <RandomiaGaming@gmail.com>
2024-01-25 15:15:32 -0800

    Backup commit

---
+++ GlobalHotkey.cs
@@ -0,0 +1,374 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System;
+using System.Threading;
+
+public delegate void HotkeyEvent();
+[Flags]
+public enum HotkeyModifier : uint
+{
+	None = 0x0000,
+	Alt = 0x0001,
+	Control = 0x0002,
+	Shift = 0x0004,
+	Windows = 0x0008,
+	NoRepeat = 0x4000,
+};
+public sealed class GlobalHotkey
+{
+	#region Private Subclasses
+	private sealed class SubsystemWindow : NativeWindow
+	{
+		protected override void WndProc(ref Message message)
+		{
+			base.WndProc(ref message);
+
+			if (message.Msg is 0x0312)
+			{
+				Keys key = (Keys)(((int)message.LParam >> 16) & 0xFFFF);
+				HotkeyModifier modifiers = (HotkeyModifier)((uint)message.LParam & 0xFFFF);
+
+				lock (_registryLock)
+				{
+					foreach (GlobalHotkey globalHotkey in _registery)
+					{
+						if (globalHotkey._key == key && globalHotkey._modifiers == modifiers)
+						{
+							globalHotkey._hotkeyEvent.Invoke();
+						}
+					}
+				}
+			}
+		}
+	}
+	#endregion
+	#region Private Static Variables
+	private static object _registryLock = new object();
+	private static List<GlobalHotkey> _registery = new List<GlobalHotkey>();
+	private static List<GlobalHotkey> _registerQue = new List<GlobalHotkey>();
+	private static List<GlobalHotkey> _unregisterQue = new List<GlobalHotkey>();
+
+	private static object _nextIDLock = new object();
+	private static int _nextID = 0;
+
+	private static object _subsystemLock = new object();
+	private static bool _subsystemRunning = false;
+	private static SubsystemWindow _subsystemWindow = null;
+	#endregion
+	#region Private Static Methods
+	private static void StartSubsystemIfNeeded()
+	{
+		lock (_subsystemLock)
+		{
+			if (!_subsystemRunning)
+			{
+				Thread _subsystemThread = new Thread(() =>
+				{
+					lock (_registryLock)
+					{
+						_subsystemWindow = new SubsystemWindow();
+						_subsystemWindow.CreateHandle(new CreateParams());
+					}
+
+					_subsystemRunning = true;
+
+					while (true)
+					{
+						lock (_registryLock)
+						{
+							while (_registerQue.Count > 0)
+							{
+								RegisterHotKey(_subsystemWindow.Handle, _registerQue[0]._hotkeyID, (uint)_registerQue[0]._modifiers, (uint)_registerQue[0]._key);
+								_registery.Add(_registerQue[0]);
+								_registerQue.RemoveAt(0);
+							}
+						}
+
+						Application.DoEvents();
+					}
+				});
+
+				_subsystemThread.Name = "GlobalHotkey Message Pump";
+				_subsystemThread.Priority = ThreadPriority.Normal;
+				_subsystemThread.IsBackground = true;
+				_subsystemThread.Start();
+
+				while (!_subsystemRunning)
+				{
+
+				}
+			}
+		}
+	}
+	[DllImport("user32.dll")]
+	private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+	[DllImport("user32.dll")]
+	private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+	#endregion
+	#region Public Variables
+	public HotkeyEvent HotkeyEvent
+	{
+		get
+		{
+			return _hotkeyEvent;
+		}
+	}
+	public Keys Key
+	{
+		get
+		{
+			return _key;
+		}
+	}
+	public HotkeyModifier Modifiers
+	{
+		get
+		{
+			return _modifiers;
+		}
+	}
+	public bool Control
+	{
+		get
+		{
+			if ((_modifiers & HotkeyModifier.Control) == HotkeyModifier.Control)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	public bool Shift
+	{
+		get
+		{
+			if ((_modifiers & HotkeyModifier.Shift) == HotkeyModifier.Shift)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	public bool Alt
+	{
+		get
+		{
+			if ((_modifiers & HotkeyModifier.Alt) == HotkeyModifier.Alt)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	public bool Windows
+	{
+		get
+		{
+			if ((_modifiers & HotkeyModifier.Windows) == HotkeyModifier.Windows)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	public bool NoRepeat
+	{
+		get
+		{
+			if ((_modifiers & HotkeyModifier.NoRepeat) == HotkeyModifier.NoRepeat)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+	public int HotkeyID
+	{
+		get
+		{
+			return _hotkeyID;
+		}
+	}
+	public bool Registered
+	{
+		get
+		{
+			lock (_localRegistryLock)
+			{
+				return _registered;
+			}
+		}
+	}
+	#endregion
+	#region Private Variables
+	private HotkeyEvent _hotkeyEvent = null;
+	private Keys _key = Keys.None;
+	private HotkeyModifier _modifiers = HotkeyModifier.None;
+	private int _hotkeyID = 0;
+	private object _localRegistryLock = new object();
+	private bool _registered = false;
+	#endregion
+	#region Public Constructors
+	public GlobalHotkey(Keys key, HotkeyEvent hotkeyEvent)
+	{
+		if (hotkeyEvent is null)
+		{
+			throw new Exception("hotkeyEvent cannot be null.");
+		}
+		_hotkeyEvent = hotkeyEvent;
+		_key = key;
+		_modifiers = HotkeyModifier.None;
+
+		lock (_nextIDLock)
+		{
+			_hotkeyID = _nextID;
+			_nextID++;
+		}
+
+		_registered = false;
+	}
+	public GlobalHotkey(Keys key, HotkeyModifier modifiers, HotkeyEvent hotkeyEvent)
+	{
+		if (hotkeyEvent is null)
+		{
+			throw new Exception("hotkeyEvent cannot be null.");
+		}
+		_hotkeyEvent = hotkeyEvent;
+		_key = key;
+		_modifiers = modifiers;
+
+		lock (_nextIDLock)
+		{
+			_hotkeyID = _nextID;
+			_nextID++;
+		}
+
+		_registered = false;
+	}
+	public GlobalHotkey(Keys key, bool control, bool shift, bool alt, bool windows, HotkeyEvent hotkeyEvent)
+	{
+		if (hotkeyEvent is null)
+		{
+			throw new Exception("hotkeyEvent cannot be null.");
+		}
+		_hotkeyEvent = hotkeyEvent;
+		_key = key;
+
+		if (control)
+		{
+			_modifiers |= HotkeyModifier.Control;
+		}
+		if (shift)
+		{
+			_modifiers |= HotkeyModifier.Shift;
+		}
+		if (alt)
+		{
+			_modifiers |= HotkeyModifier.Alt;
+		}
+		if (windows)
+		{
+			_modifiers |= HotkeyModifier.Windows;
+		}
+
+		lock (_nextIDLock)
+		{
+			_hotkeyID = _nextID;
+			_nextID++;
+		}
+
+		_registered = false;
+	}
+	public GlobalHotkey(Keys key, bool control, bool shift, bool alt, bool windows, bool noRepeat, HotkeyEvent hotkeyEvent)
+	{
+		if (hotkeyEvent is null)
+		{
+			throw new Exception("hotkeyEvent cannot be null.");
+		}
+		_hotkeyEvent = hotkeyEvent;
+		_key = key;
+
+		if (control)
+		{
+			_modifiers |= HotkeyModifier.Control;
+		}
+		if (shift)
+		{
+			_modifiers |= HotkeyModifier.Shift;
+		}
+		if (alt)
+		{
+			_modifiers |= HotkeyModifier.Alt;
+		}
+		if (windows)
+		{
+			_modifiers |= HotkeyModifier.Windows;
+		}
+		if (noRepeat)
+		{
+			_modifiers |= HotkeyModifier.NoRepeat;
+		}
+
+		lock (_nextIDLock)
+		{
+			_hotkeyID = _nextID;
+			_nextID++;
+		}
+
+		_registered = false;
+	}
+	#endregion
+	#region Public Methods
+	public void Register()
+	{
+		StartSubsystemIfNeeded();
+
+		lock (_localRegistryLock)
+		{
+			if (_registered)
+			{
+				throw new Exception("GlobalHotkey has already been registered.");
+			}
+
+			lock (_registryLock)
+			{
+				_registerQue.Add(this);
+				_registered = true;
+			}
+		}
+	}
+	public void Unregister()
+	{
+		lock (_localRegistryLock)
+		{
+			if (!_registered)
+			{
+				throw new Exception("GlobalHotkey has already been unregistered.");
+			}
+
+			lock (_registryLock)
+			{
+				_registery.Remove(this);
+				UnregisterHotKey(_subsystemWindow.Handle, _hotkeyID);
+				_registered = false;
+			}
+		}
+	}
+	#endregion
+}
---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing.Drawing2D;
+
+public static class Program
+{
+	private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+	private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+	[DllImport("user32.dll")]
+	private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+	public static void MouseDown()
+	{
+		mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+	}
+	public static void MouseUp()
+	{
+		mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+	}
+	private const int MOUSEEVENTF_WHEEL = 0x0800;
+	public static void ScrollMouseWheel(int linesToScroll)
+	{
+		int scrollAmount = linesToScroll * 120; // Each line is typically 120 units of scroll
+		mouse_event(MOUSEEVENTF_WHEEL, 0, 0, scrollAmount, 0);
+	}
+	public static bool enabled = false;
+	public static void Main()
+	{
+		string[] folders = Directory.GetDirectories("D:\\Nintendo\\ROMs");
+		foreach (string folder in folders)
+		{
+			string launchCommand;
+			string romPath;
+
+			if (ConsoleIs(folder, "NS"))
+			{
+				launchCommand = GenerateStartCommand("Yuzu");
+
+				romPath = SelectFile(folder, ".xci", ".nsp");
+			}
+			else if (ConsoleIs(folder, "NDS"))
+			{
+				launchCommand = GenerateStartCommand("DeSmuMe");
+
+				romPath = SelectFile(folder, ".nds");
+			}
+			else if (ConsoleIs(folder, "3DS"))
+			{
+				launchCommand = GenerateStartCommand("Citra", "Citra-QT.exe");
+
+				romPath = SelectFile(folder, ".3ds", ".cia");
+			}
+			else if (ConsoleIs(folder, "NES", "SNES", "GB"))
+			{
+				launchCommand = GenerateStartCommand("Mesen");
+
+				romPath = SelectFile(folder, ".nes", ".sfc", ".gb");
+			}
+			else if (ConsoleIs(folder, "GBA"))
+			{
+				launchCommand = GenerateStartCommand("mGBA");
+
+				romPath = SelectFile(folder, ".gba");
+			}
+			else
+			{
+				Console.WriteLine("Invalid folder at " + folder);
+				goto SkipMarker;
+			}
+
+			File.WriteAllText(Path.Combine($"D:\\Nintendo\\{new DirectoryInfo(folder).Name}.bat"), launchCommand + $" \"{romPath.Substring("D:\\Nintendo\\".Length)}\"");
+
+		SkipMarker:;
+		}
+
+		Console.ReadLine();
+	}
+	public static bool ConsoleIs(string folderPath, params string[] consoles)
+	{
+		try
+		{
+			string targetConsole = folderPath;
+			targetConsole = new DirectoryInfo(folderPath).Name;
+			targetConsole = targetConsole.Split('-')[0];
+			targetConsole = targetConsole.Substring(0, targetConsole.Length - 1);
+			targetConsole = targetConsole.ToUpper();
+
+			foreach (string console in consoles)
+			{
+				if (targetConsole == console.ToUpper())
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		catch
+		{
+			return false;
+		}
+	}
+	public static string SelectFile(string folderPath, params string[] extensions)
+	{
+		foreach (string extension in extensions)
+		{
+			try
+			{
+				return Directory.GetFiles(folderPath, "*" + extension)[0];
+			}
+			catch
+			{
+
+			}
+		}
+		throw new Exception("No file with desired extension was found.");
+	}
+	public static string GenerateStartCommand(string emulatorName, string executableName = null)
+	{
+		if (executableName is null)
+		{
+			return $"start \"{emulatorName}\" \"Emulators\\{emulatorName}\\{emulatorName}.exe\"";
+		}
+		else
+		{
+			return $"start \"{emulatorName}\" \"Emulators\\{emulatorName}\\{executableName}\"";
+		}
+	}
+}
